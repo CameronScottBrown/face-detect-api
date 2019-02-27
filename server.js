6b2770a93a3bb2572395d875205239601bc0bc10
Cameron Brown <cameron.scott.brown@gmail.com>
2019-02-27 14:49:53 -0800

    working on routing

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const app = express();
 
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+
 app.get('/', (req, res) => {
   res.send(
     'this is working'
   );
+});
+
+app.post('/signin', (req, res) => {
+  
+  res.json('signing in');
+});
+
+app.post('/register', (req, res) => {
+  res.json('registering');
 });
 
 app.listen(3000, () => {
@@ -19,7 +50,7 @@ app.listen(3000, () => {
     * /signin           --> POST: success/fail
     * /register         --> POST: user data
     * /profile/:userId  --> GET:  user data
-    * 
+    * /image            --> PUT:  user data
     * 
     * 
 */
