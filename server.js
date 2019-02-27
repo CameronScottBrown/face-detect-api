e9a356ae0670eb30dfdc60b1b8bfa06e639ba5b0
Cameron Brown <cameron.scott.brown@gmail.com>
2019-02-27 11:34:16 -0800

    initial commit, server in the works

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(
+    'this is working'
+  );
+});
+
+app.listen(3000, () => {
+  console.log('server running on port 3000');
+});
+
+/*
+  Planning the API routes:
+    * 
+    * 
+    * 
+    * 
+    * 
+    * 
+    * 
+*/
