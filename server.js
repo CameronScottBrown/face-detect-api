f4740be77a69e98a8cbbcd0a2f29359edbb9d67c
Cam <cameron.scott.brown@gmail.com>
2019-02-28 21:56:31 -0800

    server is working with signin and register routes connected to frontend

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
 const app = express();
 
+app.use(bodyParser.json());
+app.use(cors());
 
-const database = {
+const database = { 
   users: [
     {
       id: '123',
@@ -20,27 +27,98 @@ const database = {
       entries: 0,
       joined: new Date()
     }
+  ],
+  login: [
+    {
+      id: '987',
+      hash: '',
+      email: 'john@gmail.com'
+    }
   ]
 }
 
 app.get('/', (req, res) => {
-  res.send(
-    'this is working'
-  );
+  res.send(database.users );
 });
 
 app.post('/signin', (req, res) => {
-  
-  res.json('signing in');
+  // Load hash from your password DB.
+  bcrypt.compare(req.password, '$2a$10$m13QXFIQwJKY1v6VYhL9O.D8VnviYiKp2O/d0BWU7/L7P.s8E209G', function(err, res) {
+    console.log('match');
+  });
+  if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+    res.json('success');
+  } else {
+    res.status(400).json('error logging in'); 
+  }
 });
 
 app.post('/register', (req, res) => {
-  res.json('registering');
+  const { name, email, password } = req.body;
+  bcrypt.hash(password, null, null, function(err, hash) {
+    // Store hash in your password DB.
+    console.log(hash);
+  }); 
+  database.users.push({
+      id: '125',
+      name: name,
+      email: email,
+      password: password,
+      entries: 0,
+      joined: new Date()
+  });
+
+  res.json(database.users[database.users.length-1]);
+
 });
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+
+  database.users.forEach(user => {
+    if(user.id === id) {
+      found = true;
+      return res.json(user);
+    } 
+  });
+
+  if(!found) {
+    res.status(400).json('user not found');
+  }
+
+});
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+
+  database.users.forEach(user => {
+    if(user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    } 
+  });
+
+  if(!found) {
+    res.status(400).json('user not found');
+  }
+});
+
+
+
+
+
+// bcrypt.compare("veggies", hash, function(err, res) {
+//   // res = false
+// });
 
 app.listen(3000, () => {
   console.log('server running on port 3000');
 });
+
+
 
 /*
   Planning the API routes:
